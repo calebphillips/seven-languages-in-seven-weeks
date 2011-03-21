2156796d6bb4be1ea271bd61c5f2f664671b5050
calebphillips <caleb.phillips@gmail.com>
2011-03-21 15:01:13 -0500

    added function to display success or error tuple

--- erlang/caleb.erl
+++ erlang/caleb.erl
@@ -1,6 +1,7 @@
 -module(caleb).
 -export([count_words/1]).
 -export([count_to_ten/0]).
+-export([s_or_f/1]).
 
 count_em([]) -> 0;
 count_em([_|Rest]) -> 1 + count_em(Rest).
@@ -10,3 +11,6 @@ count_words(Sentence) -> count_em(string:tokens(Sentence, " ")).
 count_to_ten() -> count_to_ten(1).
 count_to_ten(10) -> io:format("~w~n", [10]);
 count_to_ten(Number) -> io:format("~w~n", [Number]), count_to_ten(Number+1).
+
+s_or_f(success) -> io:format("success~n", []);
+s_or_f({error, Message}) -> io:format("error: ~s~n", [Message]).
