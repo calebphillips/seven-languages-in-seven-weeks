4199afd7d3478b416e3d72d3e4f4ac8410c1c568
calebphillips <caleb.phillips@gmail.com>
2011-03-21 14:54:37 -0500

    counting to ten

--- erlang/caleb.erl
+++ erlang/caleb.erl
@@ -1,7 +1,12 @@
 -module(caleb).
 -export([count_words/1]).
+-export([count_to_ten/0]).
 
 count_em([]) -> 0;
 count_em([_|Rest]) -> 1 + count_em(Rest).
-
 count_words(Sentence) -> count_em(string:tokens(Sentence, " ")).
+
+
+count_to_ten() -> count_to_ten(1).
+count_to_ten(10) -> io:format("~w~n", [10]);
+count_to_ten(Number) -> io:format("~w~n", [Number]), count_to_ten(Number+1).
